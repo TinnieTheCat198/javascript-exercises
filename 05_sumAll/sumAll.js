e6bb2e5f4fd2638d71c755507ae10ec376112073
TinnieTheCat198 <tinnie2004@gmail.com>
2024-03-15 15:50:01 +0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum,secondNum) {
+    if (typeof firstNum !== 'number' || typeof secondNum !== 'number')
+        return 'ERROR';
+    if (firstNum < 0 || secondNum < 0)
+        return 'ERROR';
+    let upperBound, lowerBound;
+    if (firstNum < secondNum){
+        lowerBound = firstNum;
+        upperBound = secondNum;
+    }
+    else {
+        lowerBound = secondNum;
+        upperBound = firstNum;
+    }
+    let sum = 0;
+    for(let num = lowerBound; num <= upperBound; num++){
+        sum += num;
+    }
+    return sum;
 };
 
 // Do not edit below this line
