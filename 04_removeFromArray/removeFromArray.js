a1dbf67000bc99b80bedae3bd195d65a8408e194
TinnieTheCat198 <tinnie2004@gmail.com>
2024-03-15 15:17:56 +0700

    IMPORTANT: Complete exercise 4
    A special case that we cound insert more agruments into the fucntion.
    For more information, check out MDN's page on [function arguments](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments).

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let lengthArray = array.length;
+    let argumentsLength = arguments.length;
 
+    for(let deletedNumIndex = 1; deletedNumIndex < argumentsLength; deletedNumIndex++){
+        let deletedNum = arguments[deletedNumIndex];
+        for(let i = 0; i < lengthArray; i++){
+            if (deletedNum === array[i]){
+                for(let j = i; j < lengthArray - 1; j++){
+                    array[j] = array[j+1];
+                }
+                array.pop();
+                --lengthArray;
+                --i;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
