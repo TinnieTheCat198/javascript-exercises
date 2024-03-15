caacf619cecb49ce07d41195249f4280c9e15455
TinnieTheCat198 <tinnie2004@gmail.com>
2024-03-15 12:47:30 +0700

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  const chars = word.split('');
+  let length = chars.length;
+  let reversed = '';
+  for(let i = length - 1; i > -1; i-- ){
+    reversed += chars[i];
+  }
+  return reversed;
 };
 
 // Do not edit below this line
